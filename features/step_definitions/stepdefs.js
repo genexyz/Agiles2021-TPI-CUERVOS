b9539b8f3a71dcd582732e0c8721c2b79a5fbc9d
Gianluca Pagano <gpagano@primerosystems.com>
2022-05-12 00:41:21 -0300

    Final Commit

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -8,18 +8,18 @@ const screen = {
   height: 480,
 };
 
-// Given general para los 4 escenarios, no es necesario escribirlo en cada uno
 let driver = new Builder()
   .forBrowser("chrome")
   .setChromeOptions(new chrome.Options().headless().windowSize(screen))
   .build();
 
+// Given general para los 4 escenarios, no es necesario escribirlo en cada uno
 Given("Me quedan vidas restantes", async function () {
   await driver.get("https://agiles2021-tpi-cuervos.vercel.app/");
 
   await driver.findElement(By.id("formUsername")).sendKeys("Usuario");
 
-  await driver.findElement(By.className("btn-primary")).click();
+  await driver.findElement(By.xpath("//*[text()='Confirmar']")).click();
 
   let lives = await driver.findElement(By.id("vidas")).getText();
 
@@ -30,46 +30,55 @@ Given("Me quedan vidas restantes", async function () {
 When("Intento CUERVOS como la palabra secreta", async function () {
   await driver.findElement(By.id("formWord")).sendKeys("cuervos");
 
-  // await driver.findElement(By.linkText("Confirmar Palabra")).click();
-
-  return "success";
+  await driver.findElement(By.xpath("//*[text()='Confirmar Palabra']")).click();
 });
-Then("Se me informa que he ganado", function () {
-  // Escribir codigo que torne la frase superior en acciones concretas
-  return "success";
+Then("Se me informa que he ganado", async function () {
+  await driver.findElement(By.xpath("//*[text()='VICTORIA!']"));
 });
 
 // Escenario 2: Falla la Palabra Secreta
-When("Intento MARATON como la palabra secreta", function () {
-  return "success";
+When("Intento MARATON como la palabra secreta", async function () {
+  await driver.findElement(By.id("formWord")).sendKeys("maraton");
+
+  await driver.findElement(By.xpath("//*[text()='Confirmar Palabra']")).click();
 });
 
-Then("Se me informa que he perdido", function () {
-  return "success";
+Then("Se me informa que he perdido", async function () {
+  await driver.findElement(By.xpath("//*[text()='DERROTA!']"));
 });
 
 // Escenario 3: Adivina una letra de la palabra secreta
-When("Ingreso la letra C como letra de la palabra secreta", function () {
-  return "success";
+When("Ingreso la letra C como letra de la palabra secreta", async function () {
+  await driver.findElement(By.id("formLetter")).sendKeys("c");
+
+  await driver.findElement(By.xpath("//*[text()='Confirmar Letra']")).click();
 });
 
-Then("Se agrega la letra a la pista actual en su posicion correcta", function () {
-  return "success";
+Then("Se agrega la letra a la pista actual en su posicion correcta", async function () {
+  let hint = await driver.findElement(By.className("pista")).getText();
+
+  assert(hint === "C______");
 });
 
 // Escenario 4: Falla una letra de la palabra secreta
-When("Ingreso la letra F como letra de la palabra secreta", function () {
-  return "success";
+When("Ingreso la letra F como letra de la palabra secreta", async function () {
+  await driver.findElement(By.id("formLetter")).sendKeys("f");
+
+  await driver.findElement(By.xpath("//*[text()='Confirmar Letra']")).click();
 });
 
-Then("Se agrega la letra a la lista de letras incorrectas", function () {
-  return "success";
+Then("Se agrega la letra a la lista de letras incorrectas", async function () {
+  let wrongLetter = await driver.findElement(By.className("stats-letters")).getText();
+
+  assert(wrongLetter === "F");
 });
 
-Then("Se disminuye la puntuacion actual", function () {
-  return "success";
+Then("Se disminuye la puntuacion actual", async function () {
+  await driver.findElement(By.xpath("//*[text()='85']"));
 });
 
-Then("Se pierde 1 vida", function () {
-  return "success";
+Then("Se pierde 1 vida", async function () {
+  let lives = await driver.findElement(By.id("vidas")).getText();
+
+  assert(lives === "6");
 });
